6b12a00f0a6778158bd36294ce3185766ea1b93b
shrabro <shrabro1@in.ibm.com>
2019-07-12 10:02:59 +0530

    sixth task

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,20 +1,21 @@
 package com.stackroute;
 
 import com.stackroute.domain.Movie;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 
         Movie movie = context.getBean("movie", Movie.class);
 
         System.out.println(movie.getActor());
+        context.close();
 
-        
+
 
     }
 
